039b98bf0bd088cc4b57fea5498e4dbd1248bae5
Ibrahim Adepoju <ibrahim@tyms.io>
2024-09-06 16:04:23 +0100

    Version 1.0.5 commit build commit

--- dist/index.js
+++ dist/index.js
@@ -13,12 +13,22 @@ var Lehjah = function Lehjah(_ref) {
     token = _ref.token,
     _ref$environment = _ref.environment,
     environment = _ref$environment === void 0 ? 'sandbox' : _ref$environment,
+    _ref$variable = _ref.variable1,
+    variable1 = _ref$variable === void 0 ? '' : _ref$variable,
+    _ref$variable2 = _ref.variable2,
+    variable2 = _ref$variable2 === void 0 ? '' : _ref$variable2,
+    _ref$variable3 = _ref.variable3,
+    variable3 = _ref$variable3 === void 0 ? '' : _ref$variable3,
+    _ref$variable4 = _ref.variable4,
+    variable4 = _ref$variable4 === void 0 ? '' : _ref$variable4,
+    _ref$variable5 = _ref.variable5,
+    variable5 = _ref$variable5 === void 0 ? '' : _ref$variable5,
     _ref$width = _ref.width,
     width = _ref$width === void 0 ? '100%' : _ref$width,
     _ref$height = _ref.height,
     height = _ref$height === void 0 ? '100%' : _ref$height;
   var baseUrl = environment === 'sandbox' ? 'https://sandbox-widget.lehjah.com' : 'https://widget.lehjah.com';
-  var iframeSrc = "".concat(baseUrl, "/v1/app/").concat(public_key, "?business_id=").concat(business_id, "&token=").concat(token);
+  var iframeSrc = "".concat(baseUrl, "/v1/app/").concat(public_key, "?business_id=").concat(business_id, "&token=").concat(token, "&variable1=").concat(variable1, "&variable2=").concat(variable2, "&variable3=").concat(variable3, "&variable4=").concat(variable4, "&variable5=").concat(variable5);
   return /*#__PURE__*/_react["default"].createElement("iframe", {
     src: iframeSrc,
     width: width,
@@ -34,6 +44,11 @@ Lehjah.propTypes = {
   public_key: _propTypes["default"].string.isRequired,
   token: _propTypes["default"].string.isRequired,
   environment: _propTypes["default"].oneOf(['sandbox', 'production']),
+  variable1: _propTypes["default"].any,
+  variable2: _propTypes["default"].any,
+  variable3: _propTypes["default"].any,
+  variable4: _propTypes["default"].any,
+  variable5: _propTypes["default"].any,
   width: _propTypes["default"].string,
   height: _propTypes["default"].string
 };
